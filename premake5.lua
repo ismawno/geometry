162a32a3a6d77889b5f87c67ca23fe19182618e9
Ismael Bueno <ismaelwno@gmail.com>
2023-03-23 20:02:01 +0100

    Refactored premake files so that the config details are in the workspace

--- premake5.lua
+++ premake5.lua
@@ -10,33 +10,3 @@ project "geometry"
    files {"src/**.cpp", "include/**.hpp"}
 
    includedirs {"../**/include", "/opt/homebrew/Cellar/sfml/2.5.1_2/include"}
-
-   filter "configurations:debug"
-      defines { "DEBUG" }
-      runtime "Debug"
-      symbols "On"
-
-   filter "configurations:release"
-      defines { "NDEBUG" }
-      runtime "Release"
-      optimize "On"
-
-   filter "configurations:test"
-      defines { "DEBUG" }
-      runtime "Debug"
-      symbols "On"
-
-   filter "configurations:debug-profile"
-      defines { "DEBUG", "PERF" }
-      runtime "Debug"
-      symbols "On"
-
-   filter "configurations:release-profile"
-      defines { "NDEBUG", "PERF" }
-      runtime "Release"
-      optimize "On"
-
-   filter "configurations:test-profile"
-      defines { "DEBUG", "PERF" }
-      runtime "Debug"
-      symbols "On"
