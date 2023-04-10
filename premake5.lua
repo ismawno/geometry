290bb74770886c76795e8f0e9d9e37a62ffd0d79
Ismael Bueno <ismaelwno@gmail.com>
2023-04-10 01:52:57 +0100

    Ensuring staticruntime off

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "shapes-2D"
    language "C++"
    cppdialect "C++17"
    
-   kind "StaticLib"
    staticruntime "off"
+   kind "StaticLib"
 
    targetdir("bin/" .. outputdir)
    objdir("build/" .. outputdir)
