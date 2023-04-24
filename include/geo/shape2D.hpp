d6170ddd0ee89b47c8c721cb93b1464c232e0b99
Ismael Bueno <ismaelwno@gmail.com>
2023-04-24 16:37:38 +0200

    Added abstract bounding box method

--- include/geo/shape2D.hpp
+++ include/geo/shape2D.hpp
@@ -3,6 +3,7 @@
 
 #include "glm/vec2.hpp"
 #include "ini/saveable.hpp"
+#include "geo/aabb2D.hpp"
 
 namespace geo
 {
@@ -18,6 +19,7 @@ namespace geo
         virtual bool contains_point(const glm::vec2 &p) const = 0;
         virtual bool contains_origin() const = 0;
 
+        virtual aabb2D bounding_box() const = 0;
         virtual glm::vec2 closest_direction_from(const glm::vec2 &p) const = 0;
 
         const glm::vec2 &centroid() const;
