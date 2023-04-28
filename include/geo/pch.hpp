3542cd08da545e93a3ba1a1ad472c1f6bd0d6fb9
Ismael Bueno <ismaelwno@gmail.com>
2023-04-29 01:14:52 +0200

    Added pch files

---
+++ include/geo/pch.hpp
@@ -0,0 +1,17 @@
+#ifndef GEO_PCH_HPP
+#define GEO_PCH_HPP
+
+#include <glm/vec2.hpp>
+#include <vector>
+#include <utility>
+#include <array>
+#include <limits>
+#include <glm/geometric.hpp>
+#include <glm/gtx/norm.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/mat2x2.hpp>
+#include <algorithm>
+#include <cstdint>
+#include <cmath>
+
+#endif
