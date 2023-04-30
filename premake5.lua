df0024fff0fa35aa2cc25630645415d9055a1274
Ismael Bueno <ismaelwno@gmail.com>
2023-04-30 23:58:06 +0200

    Formatted premake5 lua files

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,31 @@
 project "shapes-2D"
-   language "C++"
-   cppdialect "C++17"
-   filter "system:macosx"
-      buildoptions {"-Wall", "-Wextra", "-Wpedantic", "-Wconversion", "-Wno-unused-parameter"}
-   filter{}
+language "C++"
+cppdialect "C++17"
+filter "system:macosx"
+buildoptions {
+   "-Wall",
+   "-Wextra",
+   "-Wpedantic",
+   "-Wconversion",
+   "-Wno-unused-parameter"
+}
+filter {}
 
-   pchheader "geo/pch.hpp"
-   pchsource "src/pch.cpp"
-   
-   staticruntime "off"
-   kind "StaticLib"
+pchheader "geo/pch.hpp"
+pchsource "src/pch.cpp"
 
-   targetdir("bin/" .. outputdir)
-   objdir("build/" .. outputdir)
+staticruntime "off"
+kind "StaticLib"
 
-   files {"src/**.cpp", "include/**.hpp"}
+targetdir("bin/" .. outputdir)
+objdir("build/" .. outputdir)
 
-   includedirs {"../**/include", "../vendor/glm"}
+files {
+   "src/**.cpp",
+   "include/**.hpp"
+}
+
+includedirs {
+   "../**/include",
+   "../vendor/glm"
+}
