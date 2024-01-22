1b7dbcc9e40f8d2dea9879eab57fc1df6629421f
Ismael Bueno <ismaelwno@gmail.com>
2024-01-22 01:41:37 +0100

    Managed to compile

--- include/geo/shapes2D/polygon.hpp
+++ include/geo/shapes2D/polygon.hpp
@@ -13,14 +13,63 @@ namespace geo
 template <std::size_t Capacity = SIZE_MAX> class polygon final : public shape2D
 {
   public:
-    polygon(const vertices2D<Capacity> &vertices = square(1.f)) : m_globals(vertices)
+    template <kit::Iterator<glm::vec2> It> polygon(It it1, It it2) : m_vertices(it1, it2)
+    {
+        m_transform.position = initialize_properties_and_local_vertices();
+        update();
+    }
+    template <std::size_t Size>
+    polygon(const std::array<glm::vec2, Size> &vertices) : polygon(vertices.begin(), vertices.end())
+    {
+    }
+    template <std::size_t Size>
+    polygon(const kit::dynarray<glm::vec2, Size> &vertices = square(1.f)) : polygon(vertices.begin(), vertices.end())
+    {
+    }
+    polygon(std::initializer_list<glm::vec2> vertices) : m_vertices(vertices)
+    {
+        m_transform.position = initialize_properties_and_local_vertices();
+        update();
+    }
+    polygon(const std::vector<glm::vec2> &vertices) : m_vertices(vertices)
+    {
+        m_transform.position = initialize_properties_and_local_vertices();
+        update();
+    }
+    polygon(std::vector<glm::vec2> &&vertices) : m_vertices(std::move(vertices))
     {
         m_transform.position = initialize_properties_and_local_vertices();
         update();
     }
 
-    polygon(const kit::transform2D<float> &transform, const vertices2D<Capacity> &vertices = square(1.f))
-        : shape2D(transform), m_globals(vertices)
+    template <kit::Iterator<glm::vec2> It>
+    polygon(const kit::transform2D<float> &transform, It it1, It it2) : shape2D(transform), m_vertices(it1, it2)
+    {
+        initialize_properties_and_local_vertices();
+        update();
+    }
+    template <std::size_t Size>
+    polygon(const kit::transform2D<float> &transform, const std::array<glm::vec2, Size> &vertices)
+        : polygon(transform, vertices.begin(), vertices.end())
+    {
+    }
+    template <std::size_t Size>
+    polygon(const kit::transform2D<float> &transform, const kit::dynarray<glm::vec2, Size> &vertices = square(1.f))
+        : polygon(transform, vertices.begin(), vertices.end())
+    {
+    }
+    polygon(const kit::transform2D<float> &transform, std::initializer_list<glm::vec2> vertices) : m_vertices(vertices)
+    {
+        initialize_properties_and_local_vertices();
+        update();
+    }
+    polygon(const kit::transform2D<float> &transform, const std::vector<glm::vec2> &vertices) : m_vertices(vertices)
+    {
+        initialize_properties_and_local_vertices();
+        update();
+    }
+    polygon(const kit::transform2D<float> &transform, std::vector<glm::vec2> &&vertices)
+        : m_vertices(std::move(vertices))
     {
         initialize_properties_and_local_vertices();
         update();
@@ -29,23 +78,23 @@ template <std::size_t Capacity = SIZE_MAX> class polygon final : public shape2D
     glm::vec2 support_point(const glm::vec2 &direction) const override
     {
         std::size_t support = 0;
-        float max_dot = glm::dot(direction, m_globals[support] - m_global_centroid);
-        for (std::size_t i = 1; i < m_globals.size(); i++)
+        float max_dot = glm::dot(direction, m_vertices.globals[support] - m_global_centroid);
+        for (std::size_t i = 1; i < m_vertices.size(); i++)
         {
-            const float dot = glm::dot(direction, m_globals[i] - m_global_centroid);
+            const float dot = glm::dot(direction, m_vertices.globals[i] - m_global_centroid);
             if (dot > max_dot)
             {
                 max_dot = dot;
                 support = i;
             }
         }
-        return m_globals[support];
+        return m_vertices.globals[support];
     }
 
     bool is_convex() const override
     {
-        for (std::size_t i = 0; i < m_edges.size(); i++)
-            if (kit::cross2D(edge(i), edge(i + 1)) < 0.f)
+        for (std::size_t i = 0; i < m_vertices.size(); i++)
+            if (kit::cross2D(m_vertices.edges[i], m_vertices.edges[i + 1]) < 0.f)
                 return false;
 
         return true;
@@ -54,10 +103,10 @@ template <std::size_t Capacity = SIZE_MAX> class polygon final : public shape2D
     {
         KIT_ASSERT_WARN(is_convex(),
                         "Checking if a point is contained in a non convex polygon yields undefined behaviour.")
-        for (std::size_t i = 0; i < m_globals.size(); i++)
+        for (std::size_t i = 0; i < m_vertices.size(); i++)
         {
-            const glm::vec2 &normal = m_normals[i];
-            const glm::vec2 side = p - m_globals[i];
+            const glm::vec2 &normal = m_vertices.normals[i];
+            const glm::vec2 side = p - m_vertices.globals[i];
             if (glm::dot(normal, side) > 0.f)
                 return false;
         }
@@ -68,9 +117,9 @@ template <std::size_t Capacity = SIZE_MAX> class polygon final : public shape2D
     {
         float min_dist = FLT_MAX;
         glm::vec2 closest(0.f);
-        for (std::size_t i = 0; i < m_globals.size(); i++)
+        for (std::size_t i = 0; i < m_vertices.size(); i++)
         {
-            const glm::vec2 towards = towards_segment_from(m_globals[i], global(i + 1), p);
+            const glm::vec2 towards = towards_segment_from(m_vertices.globals[i], m_vertices.globals[i + 1], p);
             const float dist = glm::length2(towards);
             if (min_dist > dist)
             {
@@ -85,9 +134,9 @@ template <std::size_t Capacity = SIZE_MAX> class polygon final : public shape2D
     {
         m_aabb.min = glm::vec2(FLT_MAX);
         m_aabb.max = -glm::vec2(FLT_MAX);
-        for (std::size_t i = 0; i < m_globals.size(); i++)
+        for (std::size_t i = 0; i < m_vertices.size(); i++)
         {
-            const glm::vec2 &v = m_globals[i];
+            const glm::vec2 &v = m_vertices.globals[i];
             if (m_aabb.min.x > v.x)
                 m_aabb.min.x = v.x;
             if (m_aabb.min.y > v.y)
@@ -99,56 +148,52 @@ template <std::size_t Capacity = SIZE_MAX> class polygon final : public shape2D
         }
     }
 
-    static vertices2D<Capacity> square(const float size)
+    static kit::dynarray<glm::vec2, 4> square(const float size)
     {
         const float hsize = 0.5f * size;
-        return vertices2D<Capacity>{
+        return kit::dynarray<glm::vec2, 4>{
             {glm::vec2(-hsize, -hsize), glm::vec2(hsize, -hsize), glm::vec2(hsize, hsize), glm::vec2(-hsize, hsize)}};
     }
-    static vertices2D<Capacity> rect(const float width, const float height)
+    static kit::dynarray<glm::vec2, 4> rect(const float width, const float height)
     {
         const float hw = 0.5f * width;
         const float hh = 0.5f * height;
-        return vertices2D<Capacity>{{glm::vec2(-hw, -hh), glm::vec2(hw, -hh), glm::vec2(hw, hh), glm::vec2(-hw, hh)}};
+        return kit::dynarray<glm::vec2, 4>{
+            {glm::vec2(-hw, -hh), glm::vec2(hw, -hh), glm::vec2(hw, hh), glm::vec2(-hw, hh)}};
     }
-    static vertices2D<Capacity> ngon(const float radius, const std::uint32_t edges)
+
+    template <std::size_t MaxEdges = Capacity>
+    static kit::dynarray<glm::vec2, MaxEdges> ngon(const float radius, const std::uint32_t edges)
     {
         KIT_ASSERT_ERROR(edges >= 3, "Cannot make polygon with less than 3 edges - edges: {0}", edges)
-        KIT_ASSERT_ERROR(edges <= Capacity, "Cannot make polygon with more than {0} edges - edges: {1}", Capacity,
-                         edges)
 
-        vertices2D<Capacity> vertices{edges};
-        const float dangle = 2.f * (float)M_PI / edges;
-        for (std::size_t i = 0; i < edges; i++)
+        if constexpr (MaxEdges == SIZE_MAX)
         {
-            const float rotation = i * dangle;
-            vertices[i] = {radius * sinf(rotation), radius * cosf(rotation)};
+            std::vector<glm::vec2> vertices;
+            vertices.reserve(edges);
+            const float dangle = 2.f * (float)M_PI / edges;
+            for (std::size_t i = 0; i < edges; i++)
+            {
+                const float rotation = i * dangle;
+                vertices.emplace_back(radius * sinf(rotation), radius * cosf(rotation));
+            }
+            return vertices;
         }
-        return vertices;
+        else
+        {
+            KIT_ASSERT_ERROR(edges <= MaxEdges, "Cannot make polygon with more than {0} edges - edges: {1}", MaxEdges,
+                             edges)
+            kit::dynarray<glm::vec2, MaxEdges> vertices{edges};
+            const float dangle = 2.f * (float)M_PI / edges;
+            for (std::size_t i = 0; i < edges; i++)
+            {
+                const float rotation = i * dangle;
+                vertices[i] = {radius * sinf(rotation), radius * cosf(rotation)};
+            }
+            return vertices;
+        }
     }
 
-    const glm::vec2 &global(const std::size_t index) const
-    {
-        return m_globals[index % m_globals.size()];
-    }
-    const glm::vec2 &local(const std::size_t index) const
-    {
-        return m_locals[index % m_locals.size()];
-    }
-
-    const glm::vec2 &edge(std::size_t index) const
-    {
-        return m_edges[index % m_edges.size()];
-    }
-    const glm::vec2 &normal(std::size_t index) const
-    {
-        return m_normals[index % m_normals.size()];
-    }
-
-    std::size_t size() const
-    {
-        return m_globals.size();
-    }
     float area() const override
     {
         return m_area;
@@ -158,25 +203,11 @@ template <std::size_t Capacity = SIZE_MAX> class polygon final : public shape2D
         return m_inertia;
     }
 
-    const vertices2D<Capacity> &globals() const
+    const vertices2D<Capacity> &vertices() const
     {
-        return m_globals;
-    }
-    const vertices2D<Capacity> &locals() const
-    {
-        return m_locals;
+        return m_vertices;
     }
 
-    const vertices2D<Capacity> &edges() const
-    {
-        return m_edges;
-    }
-    const vertices2D<Capacity> &normals() const
-    {
-        return m_normals;
-    }
-
-    // Try local + global edges and normals and switch to only global edges and normals if performance issues
 #ifdef KIT_USE_YAML_CPP
     YAML::Node encode() const override
     {
@@ -189,30 +220,27 @@ template <std::size_t Capacity = SIZE_MAX> class polygon final : public shape2D
 #endif
 
   private:
-    vertices2D<Capacity> m_globals;
-    vertices2D<Capacity> m_locals;
-    vertices2D<Capacity> m_edges;
-    vertices2D<Capacity> m_normals;
-
+    vertices2D<Capacity> m_vertices;
     float m_area = 0.f;
     float m_inertia = 0.f;
 
     void on_shape_transform_update(const glm::mat3 &transform) override
     {
         shape2D::on_shape_transform_update(transform);
-        for (std::size_t i = 0; i < m_globals.size(); i++)
-            m_globals[i] = transform * glm::vec3(m_locals[i], 1.f);
-        for (std::size_t i = 0; i < m_globals.size(); i++)
+        for (std::size_t i = 0; i < m_vertices.size(); i++)
+            m_vertices.globals[i] = transform * glm::vec3(m_vertices.locals[i], 1.f);
+        for (std::size_t i = 0; i < m_vertices.size(); i++)
         {
-            m_edges[i] = global(i + 1) - m_globals[i];
-            m_normals[i] = glm::normalize(glm::vec2(m_edges[i].y, -m_edges[i].x));
+            m_vertices.edges[i] = m_vertices.globals[i + 1] - m_vertices.globals[i];
+            m_vertices.normals[i] = glm::normalize(glm::vec2(m_vertices.edges[i].y, -m_vertices.edges[i].x));
         }
     }
 
     void sort_global_vertices()
     {
-        const glm::vec2 center = center_of_vertices(m_globals);
-        const glm::vec2 reference = m_globals[0] - center;
+        const glm::vec2 center =
+            center_of_vertices(m_vertices.globals.begin(), m_vertices.globals.end(), m_vertices.size());
+        const glm::vec2 reference = m_vertices.globals[0] - center;
 
         const auto cmp = [&center, &reference](const glm::vec2 &v1, const glm::vec2 &v2) {
             const glm::vec2 dir1 = v1 - center, dir2 = v2 - center;
@@ -228,42 +256,38 @@ template <std::size_t Capacity = SIZE_MAX> class polygon final : public shape2D
                 return kit::cross2D(dir1, dir2) > 0.f;
             return det1 > 0.f;
         };
-        std::sort(m_globals.begin(), m_globals.end(), cmp);
+        std::sort(m_vertices.globals.begin(), m_vertices.globals.end(), cmp);
     }
 
     glm::vec2 initialize_properties_and_local_vertices()
     {
-        m_locals.resize(m_globals.size());
-        m_edges.resize(m_globals.size());
-        m_normals.resize(m_globals.size());
-
         sort_global_vertices();
         const glm::vec2 current_centroid = compute_center_of_mass(*this);
 
-        for (std::size_t i = 0; i < m_globals.size(); i++)
-            m_locals[i] = m_globals[i] - current_centroid;
+        for (std::size_t i = 0; i < m_vertices.size(); i++)
+            m_vertices.locals[i] = m_vertices.globals[i] - current_centroid;
 
         m_area = compute_area(*this);
         m_inertia = compute_inertia(*this);
         return current_centroid;
     }
 
-    static glm::vec2 center_of_vertices(const vertices2D<Capacity> &vertices)
+    template <kit::Iterator<glm::vec2> It> static glm::vec2 center_of_vertices(It it1, It it2, const std::size_t size)
     {
         glm::vec2 center(0.f);
-        for (const glm::vec2 &v : vertices)
-            center += v;
-        return center / (float)vertices.size();
+        for (It it = it1; it != it2; it++)
+            center += *it;
+        return center / (float)size;
     }
 
     static glm::vec2 compute_center_of_mass(const polygon &poly)
     {
-        const glm::vec2 &p1 = poly.global(0); // No locals available yet
+        const glm::vec2 &p1 = poly.m_vertices.globals[0]; // No locals available yet
         glm::vec2 num(0.f), den(0.f);
         for (std::size_t i = 1; i < poly.size() - 1; i++)
         {
-            const glm::vec2 e1 = poly.global(i) - p1;
-            const glm::vec2 e2 = poly.global(i + 1) - p1;
+            const glm::vec2 e1 = poly.m_vertices.globals[i] - p1;
+            const glm::vec2 e2 = poly.m_vertices.globals[i + 1] - p1;
 
             const float crs = std::abs(kit::cross2D(e1, e2));
             num += (e1 + e2) * crs;
@@ -275,12 +299,12 @@ template <std::size_t Capacity = SIZE_MAX> class polygon final : public shape2D
     static float compute_area(const polygon &poly)
     {
         float area = 0.f;
-        const glm::vec2 &p1 = poly.local(0);
+        const glm::vec2 &p1 = poly.m_vertices.locals[0];
 
         for (std::size_t i = 1; i < poly.size() - 1; i++)
         {
-            const glm::vec2 e1 = poly.local(i) - p1;
-            const glm::vec2 e2 = poly.local(i + 1) - p1;
+            const glm::vec2 e1 = poly.m_vertices.locals[i] - p1;
+            const glm::vec2 e2 = poly.m_vertices.locals[i + 1] - p1;
             area += std::abs(kit::cross2D(e1, e2));
         }
         return area * 0.5f;
@@ -288,13 +312,13 @@ template <std::size_t Capacity = SIZE_MAX> class polygon final : public shape2D
 
     static float compute_inertia(const polygon &poly)
     {
-        const glm::vec2 &p1 = poly.local(0);
+        const glm::vec2 &p1 = poly.m_vertices.locals[0];
         float inertia = 0.f;
 
         for (std::size_t i = 1; i < poly.size() - 1; i++)
         {
-            const glm::vec2 &p2 = poly.local(i);
-            const glm::vec2 &p3 = poly.local(i + 1);
+            const glm::vec2 &p2 = poly.m_vertices.locals[i];
+            const glm::vec2 &p3 = poly.m_vertices.locals[i + 1];
             const glm::vec2 e1 = p1 - p2;
             const glm::vec2 e2 = p3 - p2;
 
--- include/geo/shapes2D/vertices2D.hpp
+++ include/geo/shapes2D/vertices2D.hpp
@@ -1,22 +1,27 @@
 #pragma once
 
 #include "kit/container/dynarray.hpp"
+#include "kit/utility/type_constraints.hpp"
 #include <glm/vec2.hpp>
 
 namespace geo
 {
+template <std::size_t N> class polygon;
+
 template <std::size_t N>
     requires(N >= 3)
 class vertex_container2D
 {
   public:
-    template <class... Args> vertex_container2D(Args &&...args) : m_vertices(std::forward<Args>(args)...)
+    template <class... VectorArgs>
+        requires kit::NoCopyCtorOverride<vertex_container2D, VectorArgs...>
+    vertex_container2D(VectorArgs &&...args) : m_vertices(std::forward<VectorArgs>(args)...)
     {
     }
 
     const glm::vec2 &operator[](const std::size_t index) const
     {
-        return m_vertices[index];
+        return m_vertices[index % m_vertices.size()];
     }
 
     auto begin() const
@@ -28,10 +33,15 @@ class vertex_container2D
         return m_vertices.end();
     }
 
+    std::size_t size() const
+    {
+        return m_vertices.size();
+    }
+
   private:
     glm::vec2 &operator[](const std::size_t index)
     {
-        return m_vertices[index];
+        return m_vertices[index % m_vertices.size()];
     }
 
     auto begin()
@@ -44,20 +54,21 @@ class vertex_container2D
     }
 
     kit::dynarray<glm::vec2, N> m_vertices;
+
+    friend class polygon<N>;
 };
 
 template <> class vertex_container2D<SIZE_MAX>
 {
   public:
-    vertex_container2D(std::vector<glm::vec2> *vertices, const std::size_t start, const std::size_t size)
-        : m_vertices(vertices), m_start(start), m_size(size)
+    vertex_container2D(std::vector<glm::vec2> *vertices, const std::size_t start)
+        : m_vertices(vertices), m_start(start), m_size(vertices->size())
     {
     }
 
     const glm::vec2 &operator[](const std::size_t index) const
     {
-        KIT_ASSERT_ERROR(index < m_size, "Index exceeds container size: {0}", index)
-        return m_vertices->at(m_start + index);
+        return m_vertices->at(m_start + index % m_size);
     }
 
     auto begin() const
@@ -69,11 +80,15 @@ template <> class vertex_container2D<SIZE_MAX>
         return m_vertices->begin() + m_start + m_size;
     }
 
+    std::size_t size() const
+    {
+        return m_size;
+    }
+
   private:
     glm::vec2 &operator[](const std::size_t index)
     {
-        KIT_ASSERT_ERROR(index < m_size, "Index exceeds container size: {0}", index)
-        return m_vertices->at(m_start + index);
+        return m_vertices->at(m_start + index % m_size);
     }
 
     auto begin()
@@ -88,7 +103,7 @@ template <> class vertex_container2D<SIZE_MAX>
     std::vector<glm::vec2> *m_vertices;
     std::size_t m_start, m_size;
 
-    friend class polygon;
+    friend class polygon<SIZE_MAX>;
 };
 
 template <std::size_t Capacity = SIZE_MAX>
@@ -96,158 +111,73 @@ template <std::size_t Capacity = SIZE_MAX>
 class vertices2D
 {
   public:
-    vertices2D(const std::size_t size = 0) : m_size(size)
+    template <class... VectorArgs>
+        requires kit::NoCopyCtorOverride<vertices2D, VectorArgs...>
+    vertices2D(VectorArgs &&...args)
+        : globals(std::forward<VectorArgs>(args)...), locals(globals.size()), edges(globals.size()),
+          normals(globals.size())
     {
     }
 
-    template <std::size_t OtherCapacity>
-        requires(OtherCapacity <= Capacity && OtherCapacity >= 3)
-    vertices2D(const std::array<glm::vec2, OtherCapacity> &vertices) : m_size(OtherCapacity)
-    {
-        for (std::size_t i = 0; i < OtherCapacity; ++i)
-            m_vertices[i] = vertices[i];
-    }
-    template <kit::Iterator<glm::vec2> It> vertices2D(It begin, It end) : m_size(0)
-    {
-        for (auto it = begin; it != end; ++it)
-        {
-            KIT_ASSERT_ERROR(m_size < Capacity, "Vertex capacity exceeded: {0}", Capacity)
-            m_vertices[m_size++] = *it;
-        }
-        KIT_ASSERT_ERROR(m_size >= 3, "Polygon must have at least 3 vertices")
-    }
-    vertices2D(std::initializer_list<glm::vec2> vertices) : vertices2D(vertices.begin(), vertices.end())
-    {
-    }
-
-    template <std::size_t OtherCapacity> vertices2D(const vertices2D<OtherCapacity> &other) : m_size(other.size())
-    {
-        KIT_ASSERT_ERROR(other.size() <= Capacity, "Vertex capacity exceeded: {0}", Capacity)
-        for (std::size_t i = 0; i < m_size; ++i)
-            m_vertices[i] = other[i];
-    }
-
-    template <std::size_t OtherCapacity> vertices2D &operator=(const vertices2D<OtherCapacity> &other)
-    {
-        KIT_ASSERT_ERROR(other.size() <= Capacity, "Vertex capacity exceeded: {0}", Capacity)
-        m_size = other.size();
-        for (std::size_t i = 0; i < m_size; ++i)
-            m_vertices[i] = other[i];
-        return *this;
-    }
-
-    void push_back(const glm::vec2 &vertex)
-    {
-        KIT_ASSERT_ERROR(m_size < Capacity, "Vertex capacity exceeded: {0}", Capacity)
-        m_vertices[m_size++] = vertex;
-    }
-
-    const glm::vec2 &operator[](const std::size_t index) const
-    {
-        KIT_ASSERT_ERROR(index < m_vertices.size(), "Index exceeds container size: {0}", index)
-        return m_vertices[index];
-    }
-    glm::vec2 &operator[](const std::size_t index)
-    {
-        KIT_ASSERT_ERROR(index < m_vertices.size(), "Index exceeds container size: {0}", index)
-        return m_vertices[index];
-    }
-
-    auto begin() const
-    {
-        return m_vertices.begin();
-    }
-    auto end() const
-    {
-        return m_vertices.begin() + m_size;
-    }
-
-    auto begin()
-    {
-        return m_vertices.begin();
-    }
-    auto end()
-    {
-        return m_vertices.begin() + m_size;
-    }
-
-    void resize(std::size_t size)
-    {
-        m_size = size;
-    }
     std::size_t size() const
     {
-        return m_size;
+        return globals.size();
     }
 
-  private:
-    std::array<glm::vec2, Capacity> m_vertices;
-    std::size_t m_size;
+    vertex_container2D<Capacity> globals;
+    vertex_container2D<Capacity> locals;
+    vertex_container2D<Capacity> edges;
+    vertex_container2D<Capacity> normals;
 };
-
-template <typename Container>
-concept V2Vector = std::is_same_v<Container, std::vector<glm::vec2>>;
 
 template <> class vertices2D<SIZE_MAX>
 {
   public:
-    vertices2D(const std::size_t size = 0);
-
-    template <V2Vector Container> vertices2D(Container &&vertices) : m_vertices(std::forward<Container>(vertices))
+    template <class... VectorArgs>
+        requires kit::NoCopyCtorOverride<vertices2D, VectorArgs...>
+    vertices2D(VectorArgs &&...args)
+        : m_vertices(std::forward<VectorArgs>(args)...), globals(&m_vertices, 0),
+          locals(&m_vertices, m_vertices.size()), edges(&m_vertices, 2 * m_vertices.size()),
+          normals(&m_vertices, 3 * m_vertices.size())
     {
+        m_vertices.resize(4 * m_vertices.size());
+    }
+    vertices2D(const vertices2D &other)
+        : m_vertices(other.m_vertices), globals(&m_vertices, 0), locals(&m_vertices, m_vertices.size()),
+          edges(&m_vertices, 2 * m_vertices.size()), normals(&m_vertices, 3 * m_vertices.size())
+    {
+        m_vertices.resize(4 * m_vertices.size());
+    }
+    vertices2D(vertices2D &&other)
+        : m_vertices(std::move(other.m_vertices)), globals(&m_vertices, 0), locals(&m_vertices, m_vertices.size()),
+          edges(&m_vertices, 2 * m_vertices.size()), normals(&m_vertices, 3 * m_vertices.size())
+    {
+        m_vertices.resize(4 * m_vertices.size());
     }
 
-    template <std::size_t Capacity>
-    vertices2D(const std::array<glm::vec2, Capacity> &other) : m_vertices(other.begin(), other.end())
+    vertices2D &operator=(const vertices2D &other)
     {
+        m_vertices = other.m_vertices;
+        return *this;
     }
-    template <kit::Iterator<glm::vec2> It> vertices2D(It begin, It end) : m_vertices(begin, end)
+    vertices2D &operator=(vertices2D &&other)
     {
-    }
-    vertices2D(std::initializer_list<glm::vec2> vertices);
-
-    template <std::size_t OtherCapacity> vertices2D(const vertices2D<OtherCapacity> &other)
-    {
-        m_vertices.resize(other.size());
-        for (std::size_t i = 0; i < m_vertices.size(); ++i)
-            m_vertices[i] = other[i];
-    }
-
-    template <std::size_t OtherCapacity> vertices2D &operator=(const vertices2D<OtherCapacity> &other)
-    {
-        m_vertices.resize(other.size());
-        for (std::size_t i = 0; i < m_vertices.size(); ++i)
-            m_vertices[i] = other[i];
+        m_vertices = std::move(other.m_vertices);
         return *this;
     }
 
-    void push_back(const glm::vec2 &vertex);
-
-    const glm::vec2 &operator[](const std::size_t index) const;
-    glm::vec2 &operator[](const std::size_t index);
-
-    auto begin() const
+    std::size_t size() const
     {
-        return m_vertices.begin();
+        return globals.size();
     }
-    auto end() const
-    {
-        return m_vertices.end();
-    }
-
-    auto begin()
-    {
-        return m_vertices.begin();
-    }
-    auto end()
-    {
-        return m_vertices.end();
-    }
-
-    void resize(std::size_t size);
-    std::size_t size() const;
 
   private:
     std::vector<glm::vec2> m_vertices;
+
+  public:
+    vertex_container2D<SIZE_MAX> globals;
+    vertex_container2D<SIZE_MAX> locals;
+    vertex_container2D<SIZE_MAX> edges;
+    vertex_container2D<SIZE_MAX> normals;
 };
 } // namespace geo
